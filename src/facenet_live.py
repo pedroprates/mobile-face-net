86024865ef5fe2c2eb247779b72345b29675b611
Pedro Prates <phprates@me.com>
2018-09-30 11:20:30 -0300

    changes on the real time recognition code

--- src/facenet_live.py
+++ src/facenet_live.py
@@ -3,9 +3,12 @@ import imutils
 import argparse
 import sys
 from imutils.video import VideoStream, FPS
+import facenet
 import pickle
 import time
-from detect_face.face_dectector import FaceDetector
+from detect_face.face_detector import FaceDetector
+import tensorflow as tf
+import numpy as np
 
 
 def main(args):
@@ -16,6 +19,15 @@ def main(args):
     data = pickle.loads(open(args["encondings"], "rb").read())
     face_detector = cv2.CascadeClassifier(args["cascade"])
     detector = FaceDetector(args["cascade"])
+    
+    print("[LOADING] Loading the Convolutional Neural Network model...")
+    sess = tf.Session()
+    facenet.load_model(args["model"])
+
+    images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+    embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+    phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+    embedding_size = embeddings.get_shape()[1]
 
     print("[LOADING] Starting the video stream...")
     vs = VideoStream(usePiCamera=True).start()
@@ -31,10 +43,15 @@ def main(args):
 
         # Detect faces on the frame
         rects = detector.detect_faces(gray)
-        face_images = detector.extract_faces(rgb, rects)
+        nrof_faces = len(rects)
+        if nrof_faces > 0:
+            face_images = detector.extract_faces(rgb, rects)
 
-        # Recognize the images
-        
+            # Recognize the images
+            feed_dict = { images_placeholder: face_images, phase_train_placeholder: False }
+            embeddings_array = np.zeros((nrof_faces, embedding_size))
+            embeddings_array = sess.run(embeddings, feed_dict=feed_dict)
+
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             break
@@ -54,6 +71,12 @@ def parse_arguments(argv):
                         type=str,
                         default="../datasets/face-recognition-data/encodings.pickle",
                         help="Path to the serialized faces database")
+
+    parser.add_argument("--m",
+                        "-model",
+                        type=str,
+                        default="models/facenet/201820180402-114759/20180402-114759.pb",
+                        help="Path to the CNN model")
 
     return parser.parse_args(argv)
 
