c99c487eaf7da696506d1a0c39ff992076d3a7d5
Pedro Prates <phprates@me.com>
2018-09-30 11:20:03 -0300

    renaming face_detector

--- src/detect_face/face_detector.py
+++ src/detect_face/face_detector.py
@@ -1,28 +1,30 @@
 import cv2
+import numpy as np
 
 class FaceDetector:
     def __init__(self, extractor_path):
         self.path = extractor_path
         self.face_detector = cv2.CascadeClassifier(extractor_path)
 
-    def detect_faces(self, frame, scaleFactor=1.3, minNeighbours=1, minSize=(30, 30)):
+    def detect_faces(self, frame, scaleFactor=1.3, minNeighbors=1, minSize=(30, 30)):
         rects = self.face_detector.detectMultiScale(frame, 
                                                     scaleFactor=scaleFactor, 
-                                                    minNeighbours=minNeighbours,
+                                                    minNeighbors=minNeighbors,
                                                     minSize=minSize,
                                                     flags=cv2.CASCADE_SCALE_IMAGE)
 
         return rects
 
     @staticmethod
-    def extract_faces(frame, rects):
-        multiple_faces = []
+    def extract_faces(frame, rects, size=(160, 160)):
+        nrof_images = len(rects)
+        images = np.zeros((nrof_images, *size, 3), dtype=np.uint8)
 
-        for x, y, w, h in rects:
-            h_margin = 0.1*w
-            v_margin = 0.1*h
+        for idx, (x, y, w, h) in enumerate(rects):
+            h_margin = int(0.1*w)
+            v_margin = int(0.1*h)
 
             cropped_image = frame[y-v_margin:y+h+v_margin, x-h_margin:x+w+h_margin]
-            multiple_faces.append(cropped_image)
+            images[idx,:,:,:] = cv2.resize(cropped_image, size)
         
-        return multiple_faces
+        return images
