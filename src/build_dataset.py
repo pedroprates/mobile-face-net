a832a775d2075a71b35d333b813063fee5f60e13
Pedro Prates <phprates@me.com>
2018-11-29 10:00:54 -0200

    building datasets and running live on camera

--- src/build_dataset.py
+++ src/build_dataset.py
@@ -4,20 +4,38 @@ import os
 import tensorflow as tf
 import cv2
 import numpy as np
+import time
+import keras
+import keras.backend as K
+import json
 
 def main(args):
     print("[STARTING] Starting the code to create the dataset.")
     print(".\n.\n.")
-    print("[LOADING] Loading the model...")
-    model_path = os.path.expanduser(args['model'])
-    assert os.path.isfile(model_path), "The model path should be a .pb file."
-    sess = tf.Session()
-    utils.load_model(model_path)
+    
+    print("[LOADING] Loading the Convolutional Neural Network model...")
+    type_mode = args["type"]
+    assert type_mode in ["MobileFaceNet", "FaceNet"], "Only MobileFaceNet or FaceNet are supported."
 
-    images_placeholder = tf.get_default_graph().get_tensor_by_name('input:0')
-    embeddings = tf.get_default_graph().get_tensor_by_name('embeddings:0')
-    phase_train_placeholder = tf.get_default_graph().get_tensor_by_name('phase_train:0')
-    embeddings_size = embeddings.get_shape()[1]
+    if type_mode == 'FaceNet':
+        start = time.time()
+        sess = tf.Session()
+        utils.load_model(args["model"])
+
+        images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+        embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+        phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+        print("[LOADING] Loading the FaceNet weights took %.2f" % (time.time() - start))
+    else:
+        K.clear_session()
+        define_keras_functions()
+        with open(args["json"]) as f:
+            start = time.time()
+            model_json = json.load(f)
+            model = keras.models.model_from_json(model_json)
+            print("[LOADING] Loadng the Weights...")
+            model.load_weights(args["weights"])
+            print("[LOADING] Loading the MobileFaceNet weights took %.2fs" % (time.time() - start))
 
     print("[LOADING] Checking the dataset path...")
     dataset_path = args['dataset']
@@ -26,21 +44,31 @@ def main(args):
     people = [person for person in os.listdir(dataset_path) if not person.startswith('.')]
 
     print('[RUNNING] Building the dataset!')
+    times = []
     for person in people:
         print('\t[BUILD] Building ', person)
         person_path = os.path.join(dataset_path, person)
         pics = [pic for pic in os.listdir(person_path) if (pic.endswith('jpg') or pic.endswith('jpeg'))]
         nrof_pics = len(pics)
         images = np.zeros((nrof_pics, args['image'], args['image'], 3))
-        embeddings_array = np.zeros((nrof_pics, embeddings_size))
 
         for idx, pic in enumerate(pics):
             image = cv2.imread(os.path.join(person_path, pic))
+            image = cv2.resize(image, (160, 160))
             image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            images[idx, :, :, :] = image_rgb
+            images[idx, :, :, :] = image_rgb / 255
 
-        feed_dict = { images_placeholder: images, phase_train_placeholder: False }
-        embeddings_array = sess.run(embeddings, feed_dict=feed_dict)
+        # Recognize the images
+        if type_mode == 'FaceNet':
+            start_time = time.time()
+            feed_dict = {images_placeholder: images, phase_train_placeholder: False}
+            embeddings_array = sess.run(embeddings, feed_dict=feed_dict)
+            times.append(time.time() - start_time)
+        else:
+            start_time = time.time()
+            embeddings_array = model.predict(images)
+            times.append(time.time() - start_time)
+
         output_file = os.path.join(person_path, person+'.npy')
 
         if (os.path.isfile(output_file)):
@@ -60,7 +88,6 @@ def parse_arguments():
     ap.add_argument('-m',
                     '--model',
                     type=str,
-                    required=True,
                     help="Path to the CNN model")
 
     ap.add_argument('-i',
@@ -68,8 +95,41 @@ def parse_arguments():
                     type=int,
                     default=160,
                     help='Size of the image')
+
+    ap.add_argument('-t',
+                    '--type',
+                    type=str,
+                    default="MobileFaceNet",
+                    help="Which model to use to create the embeddings")
+    
+    ap.add_argument('-j',
+                    '--json',
+                    type=str,
+                    default='/home/pi/Documents/TCC/face-recognition/models/mobilefacenet/model.json',
+                    help='Path to the JSON containing the model structure')
+
+    ap.add_argument('-w',
+                    '--weights',
+                    type=str,
+                    default='/home/pi/Documents/TCC/face-recognition/models/mobilefacenet/model_weights.h5',
+                    help='Path to the weights of the model')
     
     return vars(ap.parse_args())
+
+def define_keras_functions():
+    def distillation_loss(y_true, y_pred):
+        return K.square(y_pred - y_true)
+
+    def max_diff(y_true, y_pred):
+        return K.max(K.square(y_pred - y_true), axis=-1)
+
+    def sum_diff(y_true, y_pred):
+        return K.sum(K.square(y_pred - y_true), axis=-1)
+
+    keras.losses.distillation_loss = distillation_loss
+    keras.metrics.max_diff = max_diff
+    keras.metrics.sum_diff = sum_diff
+
 
 if __name__ == "__main__":
     main(parse_arguments())
--- src/facenet_live.py
+++ src/facenet_live.py
@@ -1,41 +1,58 @@
 import cv2
 import imutils
 import argparse
-import sys
 from imutils.video import VideoStream, FPS
-import pickle
 import time
 from detect_face.face_detector import FaceDetector
 import tensorflow as tf
-import numpy as np
 import utils
-
+import keras
+import keras.backend as K
+import json
 
 def main(args):
     
     print("[STARTING] Facenet ResNet v1 for Facial Recognition")
     print(".\n.\n.")
     print("[LOADING] Loading face detector...")
-    face_detector = cv2.CascadeClassifier(args["cascade"])
     detector = FaceDetector(args["cascade"])
     
     print("[LOADING] Loading the faces dataset...")
     dataset, name_to_idx, idx_to_name = utils.build_dataset(args["dataset"])
 
     print("[LOADING] Loading the Convolutional Neural Network model...")
-    sess = tf.Session()
-    utils.load_model(args["model"])
+    type_mode = args["type"]
+    use_pi = args['run'] == 'raspberry'
+    assert type_mode in ["MobileFaceNet", "FaceNet"], "Only MobileFaceNet or FaceNet are supported."
 
-    images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
-    embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
-    phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
-    embedding_size = embeddings.get_shape()[1]
+    if type_mode == 'FaceNet':
+        start = time.time()
+        sess = tf.Session()
+        utils.load_model(args["model"])
+
+        images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+        embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+        phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+        print("[LOADING] Loading the FaceNet weights took %.2f" % (time.time() - start))
+    else:
+        K.clear_session()
+        define_keras_functions()
+        with open(args["json"]) as f:
+            start = time.time()
+            model_json = json.load(f)
+            model = keras.models.model_from_json(model_json)
+            print("[LOADING] Loadng the Weights...")
+            model.load_weights(args["weights"])
+            print("[LOADING] Loading the MobileFaceNet weights took %.2fs" % (time.time() - start))
 
     print("[LOADING] Starting the video stream...")
-    # vs = VideoStream(usePiCamera=True).start()
-    vs = VideoStream(src=0).start()
+    if use_pi:
+        vs = VideoStream(usePiCamera=True).start()
+    else:
+        vs = VideoStream(src=0).start()
     time.sleep(2.0)
     fps = FPS().start()
+    times = []
 
     while True:
         frame = vs.read()
@@ -49,17 +66,28 @@ def main(args):
         nrof_faces = len(rects)
         if nrof_faces > 0:
             face_images = detector.extract_faces(rgb, rects)
-
+            face_images = face_images / 255
             # Recognize the images
-            feed_dict = { images_placeholder: face_images, phase_train_placeholder: False }
-            embeddings_array = np.zeros((nrof_faces, embedding_size))
-            embeddings_array = sess.run(embeddings, feed_dict=feed_dict)
+            if type_mode == 'FaceNet':
+                start_time = time.time()
+                feed_dict = {images_placeholder: face_images, phase_train_placeholder: False}
+                embeddings_array = sess.run(embeddings, feed_dict=feed_dict)
+                times.append(time.time() - start_time)
+            else:
+                start_time = time.time()
+                embeddings_array = model.predict(face_images)
+                times.append(time.time() - start_time)
 
             for idx, embedding in enumerate(embeddings_array):
                 embedding = embedding.reshape((1, *embedding.shape))
-                predicted = utils.predict_face(dataset, name_to_idx, idx_to_name, embedding, distance_metric='cosine')
+                predicted = utils.predict_face(dataset,
+                                               name_to_idx,
+                                               idx_to_name,
+                                               embedding,
+                                               threshold=3,
+                                               distance_metric='cosine')
                 x, y, w, h = rects[idx]
-                color = (255, 0, 0) if predicted == "Unknown" else (0, 255, 0)
+                color = (0, 0, 255) if predicted == "Unknown" else (0, 255, 0)
                 cv2.rectangle(frame, (x, y+h), (x+w, y), color, 2)
                 top = y+h-15 if y+h-15 > 15 else y+h+15
                 cv2.putText(frame, predicted, (x, top), cv2.FONT_HERSHEY_SIMPLEX, 0.75, color, 2)
@@ -76,9 +104,11 @@ def main(args):
     fps.stop()
     print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
     print("[INFO] approximated FPS: {:.2f}fps".format(fps.fps()))
+    print("[INFO] approximated forward propagation time: {:.2f}s".format(sum(times)/len(times)))
 
     cv2.destroyAllWindows()
     vs.stop()
+
 
 def parse_arguments():
     """ Parsing arguments to run variables to the main
@@ -88,7 +118,7 @@ def parse_arguments():
     parser.add_argument("-c",
                         "--cascade",
                         type=str,
-                        default="../models/HaarCascade/haarcascade_frontalface_default.xml",
+                        default="/home/pi/Documents/TCC/face-recognition/models/haarcascade/haarcascade_frontalface_default.xml",
                         help="Path to the face cascade config files")
     parser.add_argument("-d",
                         "--dataset",
@@ -99,10 +129,50 @@ def parse_arguments():
     parser.add_argument("-m",
                         "--model",
                         type=str,
-                        default="models/facenet/201820180402-114759/20180402-114759.pb",
+                        default="/home/pi/Documents/TCC/face-recognition/models/facenet/20180402-114759.pb",
                         help="Path to the CNN model")
 
+    parser.add_argument("-t",
+                        "--type",
+                        type=str,
+                        default="MobileFaceNet",
+                        help="CNN architecture to be used")
+
+    parser.add_argument("-j",
+                        "--json",
+                        type=str,
+                        default="/home/pi/Documents/TCC/face-recognition/models/mobilefacenet/model.json",
+                        help="Path to the JSON file")
+    
+
+    parser.add_argument("-w",
+                        "--weights",
+                        type=str,
+                        default="/home/pi/Documents/TCC/face-recognition/models/mobilefacenet/model_weights.h5",
+                        help="Path to the weights")
+
+    parser.add_argument("-r",
+                        "--run",
+                        type=str,
+                        default="raspberry",
+                        help="Where to run, either Raspberry or PC")
     return vars(parser.parse_args())
+
+
+def define_keras_functions():
+    def distillation_loss(y_true, y_pred):
+        return K.square(y_pred - y_true)
+
+    def max_diff(y_true, y_pred):
+        return K.max(K.square(y_pred - y_true), axis=-1)
+
+    def sum_diff(y_true, y_pred):
+        return K.sum(K.square(y_pred - y_true), axis=-1)
+
+    keras.losses.distillation_loss = distillation_loss
+    keras.metrics.max_diff = max_diff
+    keras.metrics.sum_diff = sum_diff
+
 
 if __name__ == "__main__":
     main(parse_arguments())
